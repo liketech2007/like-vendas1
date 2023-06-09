6c70fa22756470c51de9f76921448194fc091b80
Replit user <>
2023-06-09 08:28:29 +0000

    q

---
+++ src/components/mainDashboard.tsx
@@ -0,0 +1,12 @@
+export function MainDashboard() {
+  return (
+    <main className="flex">
+    <div className="hidden lg:block">
+      oi1
+    </div>
+    <div>
+      oi
+    </div>
+    </main>
+  )
+}
